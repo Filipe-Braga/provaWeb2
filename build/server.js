962cedcf7ec0cd4cd7b6d4f072f86faf504688be
Filipe Braga <102600194+Filipe-Braga@users.noreply.github.com>
2024-06-10 10:39:13 -0300

    Add files via upload

---
+++ build/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const AuthRoutes_1 = __importDefault(require("./routes/AuthRoutes"));
+const UserRoutes_1 = __importDefault(require("./routes/UserRoutes"));
+const port = 3000;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(UserRoutes_1.default);
+app.use(AuthRoutes_1.default);
+app.listen(port, function () {
+    console.log("Servidor rodando na porta " + port);
+});
